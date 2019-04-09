a35aa15e3b24479a43168ec0250ba855d9399b08
lupyuen <luppy@appkaki.com>
2019-04-09 14:54:42 +0800

    First version

---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"sync"
+)
+
+var (
+	//  Threadsafe map that maps device ID to device state.  It needs to be threadsafe because we will reading and writing concurrently.
+	deviceStates sync.Map
+)
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("Defaulting to port %s", port)
+	}
+
+	log.Printf("Listening on port %s", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+}
+
+func testDeviceStates() {
+	//  Fetch an item that doesn't exist yet.
+	deviceID := "hello"
+	result, ok := deviceStates.Load(deviceID)
+	if ok {
+		fmt.Println(result.(string))
+	} else {
+		fmt.Println("value not found for key: `hello`")
+	}
+
+	//  Store an item in the map.
+	deviceStates.Store("hello", "world")
+	fmt.Println("added value: `world` for key: `hello`")
+
+	// Fetch the item we just stored.
+	result, ok = deviceStates.Load("hello")
+	if ok {
+		fmt.Printf("result: `%s` found for key: `hello`\n", result.(string))
+	}
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	testDeviceStates()
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "Hello, World!")
+		return
+	}
+	http.NotFound(w, r)
+}
